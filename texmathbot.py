6a47901263a96e367a77f141830fe601be8219e3
Thomas Farr <xtansia@xtansia.com>
2016-09-11 06:42:45 +1200

    Enable logging

--- texmathbot.py
+++ texmathbot.py
@@ -12,11 +12,33 @@ import aiohttp
 import discord
 from discord.ext import commands
 import json
+import logging
 import os
 import shutil
 import sys
 import tempfile
 import uuid
+
+### Logging ###
+
+log_formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s')
+
+log_file_handler = logging.FileHandler(filename='texmathbot.log', encoding='utf-8', mode='w')
+log_file_handler.setFormatter(log_formatter)
+
+log_stdout_handler = logging.StreamHandler(stream=sys.stdout)
+log_stdout_handler.setFormatter(log_formatter)
+log_stdout_handler.setLevel(logging.INFO)
+
+discord_logger = logging.getLogger('discord')
+discord_logger.setLevel(logging.DEBUG)
+discord_logger.addHandler(log_file_handler)
+discord_logger.addHandler(log_stdout_handler)
+
+logger = logging.getLogger('texmathbot')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(log_file_handler)
+logger.addHandler(log_stdout_handler)
 
 
 ### Settings ###
@@ -41,17 +63,13 @@ bot = commands.Bot(command_prefix=command_prefix, description=bot_description)
 
 @bot.event
 async def on_ready():
-  print('Logged in as')
-  print(bot.user.name)
-  print(bot.user.id)
-  print('----------')
-
+  logger.info('Logged in as %s [%s]' % (bot.user.name, bot.user.id))
 
 @bot.command()
 async def math(*, mathexpr: str):
   """Renders a LaTeX math expression to a PNG"""
 
-  print('Math Expr: `%s`' % mathexpr)
+  logger.info('math `%s`' % mathexpr)
   latex = math_template.replace('__DATA__', mathexpr)
 
   img_fn = None
@@ -64,7 +82,7 @@ async def math(*, mathexpr: str):
         with os.fdopen(img_fd, 'wb') as img_fh:
           img_fh.write(await resp.read())
       else:
-        print('Error [%s] : %s' % (mathexpr, await resp.text()))
+        logger.error('`%s`:  %s' % (mathexpr, await resp.text()))
         if resp.status == 408:
           error_msg = 'Timed Out'
         elif resp.status == 400:
